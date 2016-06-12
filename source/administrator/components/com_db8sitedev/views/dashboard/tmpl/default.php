a7ba704fc7f57d944da35e93155b3b34154b7c97
pe7er <github@pe7er.com>
2016-06-12 13:58:38 +0200

    Remove PHPStorm system directory

--- source/administrator/components/com_db8sitedev/views/dashboard/tmpl/default.php
+++ source/administrator/components/com_db8sitedev/views/dashboard/tmpl/default.php
@@ -7,50 +7,213 @@
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 
-defined('_JEXEC') or die; ?>
-<form action="<?php echo JRoute::_('index.php?option=com_db8sitedev&view=dashboard'); ?>" method="post"
-      name="adminForm" id="adminForm">
-<pre>
-     888 888       .d8888b.        .d8888b.  d8b 888                 8888888b.
-     888 888      d88P  Y88b      d88P  Y88b Y8P 888                 888  "Y88b
-     888 888      Y88b. d88P      Y88b.          888                 888    888
- .d88888 88888b.   "Y88888"        "Y888b.   888 888888 .d88b.       888    888  .d88b.  888  888
-d88" 888 888 "88b .d8P""Y8b.          "Y88b. 888 888   d8P  Y8b      888    888 d8P  Y8b 888  888
-888  888 888  888 888    888            "888 888 888   88888888      888    888 88888888 Y88  88P
-Y88b 888 888 d88P Y88b  d88P      Y88b  d88P 888 Y88b. Y8b.          888  .d88P Y8b.      Y8bd8P
- "Y88888 88888P"   "Y8888P"        "Y8888P"  888  "Y888 "Y8888       8888888P"   "Y8888    Y88P
+defined('_JEXEC') or die;
 
-</pre>
-<b>db8 Site Dev</b> v0.9.0 - A Joomla Site Development Tool<br>
-(C) 2016 by Peter Martin, <a href="https://db8.nl" title="Joomla specialist db8.nl" target="_blank">db8.nl</a>.
-All Rights Reserved. This extension has been licensed under GPL v2 and higher.<br>
-Inspired by the awesome <a href="https://github.com/renekreijveld/livechecklist" title="Live Checklist" target="_blank">Live Checklist</a>
-created by <a href="http://www.renekreijveld.nl" target="_blank">Rene Kreijveld</a>.
-<br>
-<br>
-<h1>Instructions</h1>
-<h2>Component</h2>
-<ol>
-    <li>Create some <a href="index.php?option=com_categories&extension=com_db8sitedev">Checklist Categories</a></li>
-    <li>Create a list of <a href="index.php?option=com_db8sitedev&view=checks">Check Items</a> that you want on your checklist</li>
-    <li>Manage your <a href="index.php?option=com_db8sitedev&view=checklist">Checklist</a> by toggling items on or off<br><br>
-<?php
-if (JComponentHelper::getParams('com_db8sitedev')->get('generate_site_checklist_button') == 1)
+JHtml::addIncludePath(JPATH_COMPONENT . '/helpers/');
+JHtml::_('bootstrap.tooltip');
+JHtml::_('behavior.multiselect');
+JHtml::_('formbehavior.chosen', 'select');
+
+// Import CSS
+$document = JFactory::getDocument();
+$document->addStyleSheet(JUri::root() . 'administrator/components/com_db8sitedev/assets/css/db8sitedev.css');
+$document->addStyleSheet(JUri::root() . 'media/com_db8sitedev/css/list.css');
+
+$user = JFactory::getUser();
+$userId = $user->get('id');
+$listOrder = $this->state->get('list.ordering');
+$listDirn = $this->state->get('list.direction');
+$canOrder = $user->authorise('core.edit.state', 'com_db8sitedev');
+$saveOrder = $listOrder == 'a.`ordering`';
+
+
+if($saveOrder)
 {
-	echo '-> or <a href="index.php?option=com_db8sitedev&task=dashboard.generate">[Generate Live Checklist]</a>';
+	$saveOrderingUrl = 'index.php?option=com_db8sitedev&task=checks.saveOrderAjax&tmpl=component';
+	JHtml::_('sortablelist.sortable', 'checkList', 'adminForm', strtolower($listDirn), $saveOrderingUrl);
 }
-else
-{
-	echo '(The Generate Live Checklist Option has been disabled. To generate Live Checklist, enable it under [Options])';
-}
+
+$sortFields = $this->getSortFields();
 ?>
-    </li>
-</ol>
-<h2>Module</h2>
-<ol>
-    <li>Switch on the <a href="index.php?option=com_modules&client_id=1">Site Checklist Module</a> and assign it to position <b>cpanel</b></li>
-    <li>The <a href="index.php">Admin Control Panel </a> will display a Module with an overview of the checklist.</li>
-    <li>You can click on any part to navigate to your filtered checklist</li>
-</ol>
-<br>
-</form>
+<script type="text/javascript">
+	Joomla.orderTable = function () {
+		table = document.getElementById("sortTable");
+		direction = document.getElementById("directionTable");
+		order = table.options[table.selectedIndex].value;
+		if (order != '<?php echo $listOrder; ?>') {
+			dirn = 'asc';
+		} else {
+			dirn = direction.options[direction.selectedIndex].value;
+		}
+		Joomla.tableOrdering(order, dirn, '');
+	};
+
+	jQuery(document).ready(function () {
+		jQuery('#clear-search-button').on('click', function () {
+			jQuery('#filter_search').val('');
+			jQuery('#adminForm').submit();
+		});
+	});
+
+	window.toggleField = function (id, task, field) {
+
+		var f = document.adminForm,
+			i = 0, cbx,
+			cb = f[id];
+
+		if (!cb) return false;
+
+		while (true) {
+			cbx = f['cb' + i];
+
+			if (!cbx) break;
+
+			cbx.checked = false;
+			i++;
+		}
+
+		var inputField = document.createElement('input');
+		inputField.type = 'hidden';
+		inputField.name = 'field';
+		inputField.value = field;
+		f.appendChild(inputField);
+
+		cb.checked = true;
+		f.boxchecked.value = 1;
+		window.submitform(task);
+
+		return false;
+	};
+
+</script>
+
+<?php if (!empty($this->extra_sidebar))
+{
+	$this->sidebar .= $this->extra_sidebar;
+} ?>
+
+<form action="<?php echo JRoute::_('index.php?option=com_db8sitedev&view=checks'); ?>" method="post"
+	  name="adminForm" id="adminForm">
+	<?php if (!empty($this->sidebar)): ?>
+	<div id="j-sidebar-container" class="span2">
+		<?php echo $this->sidebar; ?>
+	</div>
+	<div id="j-main-container" class="span10">
+		<?php else : ?>
+		<div id="j-main-container">
+			<?php endif; ?>
+			<?php
+			// Search tools bar
+			echo JLayoutHelper::render('joomla.searchtools.default', array('view' => $this));
+			?>
+
+			<table class="table table-striped" id="checkList">
+				<thead>
+				<tr>
+					<?php if (isset($this->items[0]->ordering)): ?>
+						<th width="1%" class="nowrap center hidden-phone">
+							<?php echo JHtml::_('grid.sort', '<i class="icon-menu-2"></i>', 'a.`ordering`', $listDirn, $listOrder, null, 'asc', 'JGRID_HEADING_ORDERING'); ?>
+						</th>
+					<?php endif; ?>
+					<th width="1%" class="hidden-phone">
+						<input type="checkbox" name="checkall-toggle" value=""
+							   title="<?php echo JText::_('JGLOBAL_CHECK_ALL'); ?>" onclick="Joomla.checkAll(this)"/>
+					</th>
+					<?php if (isset($this->items[0]->state)): ?>
+						<th width="1%" class="nowrap center">
+							<?php echo JHtml::_('grid.sort', 'JSTATUS', 'a.`state`', $listDirn, $listOrder); ?>
+						</th>
+					<?php endif; ?>
+
+					<th class='left'>
+						<?php echo JHtml::_('grid.sort', 'COM_DB8SITEDEV_CHECKS_TITLE', 'a.`title`', $listDirn, $listOrder); ?>
+					</th>
+					<th class='left'>
+						<?php echo JHtml::_('grid.sort', 'COM_DB8SITEDEV_CHECKS_CATID', 'a.`catid`', $listDirn, $listOrder); ?>
+					</th>
+					<th class='left'>
+						<?php echo JHtml::_('grid.sort', 'COM_DB8SITEDEV_CHECKS_ID', 'a.`id`', $listDirn, $listOrder); ?>
+					</th>
+
+				</tr>
+				</thead>
+				<tfoot>
+				<tr>
+					<td colspan="<?php echo isset($this->items[0]) ? count(get_object_vars($this->items[0])) : 10; ?>">
+						<?php echo $this->pagination->getListFooter(); ?>
+					</td>
+				</tr>
+				</tfoot>
+				<tbody>
+				<?php foreach ($this->items as $i => $item) :
+					$ordering = ($listOrder == 'a.ordering');
+					$canCreate = $user->authorise('core.create', 'com_db8sitedev');
+					$canEdit = $user->authorise('core.edit', 'com_db8sitedev');
+					$canCheckin = $user->authorise('core.manage', 'com_db8sitedev');
+					$canChange = $user->authorise('core.edit.state', 'com_db8sitedev');
+					?>
+					<tr class="row<?php echo $i % 2; ?>">
+
+						<?php if (isset($this->items[0]->ordering)) : ?>
+						<td class="order nowrap center hidden-phone">
+							<?php if ($canChange) :
+								$disableClassName = '';
+								$disabledLabel = '';
+
+								if (!$saveOrder) :
+									$disabledLabel = JText::_('JORDERINGDISABLED');
+									$disableClassName = 'inactive tip-top';
+								endif; ?>
+								<span class="sortable-handler hasTooltip <?php echo $disableClassName ?>"
+									  title="<?php echo $disabledLabel ?>">
+										<i class="icon-menu"></i>
+								</span>
+								<input type="text" style="display:none" name="order[]" size="5"
+									   value="<?php echo $item->ordering; ?>" class="width-20 text-area-order "/>
+							<?php else : ?>
+								<span class="sortable-handler inactive">
+									<i class="icon-menu"></i>
+								</span>
+							<?php endif; ?>
+						</td>
+						<?php endif; ?>
+
+						<td class="hidden-phone">
+							<?php echo JHtml::_('grid.id', $i, $item->id); ?>
+						</td>
+						<?php if (isset($this->items[0]->state)): ?>
+							<td class="center">
+								<?php echo JHtml::_('jgrid.published', $item->state, $i, 'checks.', $canChange, 'cb'); ?>
+							</td>
+						<?php endif; ?>
+
+						<td>
+							<?php if (isset($item->checked_out) && $item->checked_out && ($canEdit || $canChange)) : ?>
+								<?php echo JHtml::_('jgrid.checkedout', $i, $item->editor, $item->checked_out_time, 'checks.', $canCheckin); ?>
+							<?php endif; ?>
+							
+							<?php if ($canEdit) : ?>
+								<a href="<?php echo JRoute::_('index.php?option=com_db8sitedev&task=check.edit&id=' . (int) $item->id); ?>">
+									<?php echo $this->escape($item->title); ?></a>
+							<?php else : ?>
+								<?php echo $this->escape($item->title); ?>
+							<?php endif; ?>
+						</td>
+						<td>
+							<?php echo $item->catid; ?>
+						</td>
+						<td>
+							<?php echo $item->id; ?>
+						</td>
+					</tr>
+				<?php endforeach; ?>
+				</tbody>
+			</table>
+
+			<input type="hidden" name="task" value=""/>
+			<input type="hidden" name="boxchecked" value="0"/>
+			<input type="hidden" name="filter_order" value="<?php echo $listOrder; ?>"/>
+			<input type="hidden" name="filter_order_Dir" value="<?php echo $listDirn; ?>"/>
+			<?php echo JHtml::_('form.token'); ?>
+		</div>
+</form>        
--- source/administrator/components/com_db8sitedev/views/dashboard/view.html.php
+++ source/administrator/components/com_db8sitedev/views/dashboard/view.html.php
@@ -16,7 +16,7 @@ jimport('joomla.application.component.view');
  *
  * @since  1.6
  */
-class Db8sitedevViewDashboard extends JViewLegacy
+class Db8sitedevViewChecks extends JViewLegacy
 {
 	protected $items;
 
@@ -36,6 +36,10 @@ class Db8sitedevViewDashboard extends JViewLegacy
 	public function display($tpl = null)
 	{
 		$this->state = $this->get('State');
+		$this->items = $this->get('Items');
+		$this->pagination = $this->get('Pagination');
+		$this->filterForm    = $this->get('FilterForm');
+		$this->activeFilters = $this->get('ActiveFilters');
 
 		// Check for errors.
 		if (count($errors = $this->get('Errors')))
@@ -43,7 +47,7 @@ class Db8sitedevViewDashboard extends JViewLegacy
 			throw new Exception(implode("\n", $errors));
 		}
 
-		Db8sitedevHelpersDb8sitedev::addSubmenu('dashboard');
+		Db8sitedevHelpersDb8sitedev::addSubmenu('checks');
 
 		$this->addToolbar();
 
@@ -60,9 +64,68 @@ class Db8sitedevViewDashboard extends JViewLegacy
 	 */
 	protected function addToolbar()
 	{
+		$state = $this->get('State');
 		$canDo = Db8sitedevHelpersDb8sitedev::getActions();
 
-		JToolbarHelper::title(JText::_('COM_DB8SITEDEV') . ": " . JText::_('COM_DB8SITEDEV_TITLE_DASHBOARD'), 'dashboard.png');
+		JToolbarHelper::title(JText::_('COM_DB8SITEDEV') . ": " . JText::_('COM_DB8SITEDEV_TITLE_CHECKS'), 'checkbox-partial.png');
+
+		// Check if the form exists before showing the add/edit buttons
+		$formPath = JPATH_COMPONENT_ADMINISTRATOR . '/views/check';
+
+		if (file_exists($formPath))
+		{
+			if ($canDo->get('core.create'))
+			{
+				JToolbarHelper::addNew('check.add', 'JTOOLBAR_NEW');
+				JToolbarHelper::custom('checks.duplicate', 'copy.png', 'copy_f2.png', 'JTOOLBAR_DUPLICATE', true);
+			}
+
+			if ($canDo->get('core.edit') && isset($this->items[0]))
+			{
+				JToolbarHelper::editList('check.edit', 'JTOOLBAR_EDIT');
+			}
+		}
+
+		if ($canDo->get('core.edit.state'))
+		{
+			if (isset($this->items[0]->state))
+			{
+				JToolbarHelper::divider();
+				JToolbarHelper::custom('checks.publish', 'publish.png', 'publish_f2.png', 'JTOOLBAR_PUBLISH', true);
+				JToolbarHelper::custom('checks.unpublish', 'unpublish.png', 'unpublish_f2.png', 'JTOOLBAR_UNPUBLISH', true);
+			}
+			elseif (isset($this->items[0]))
+			{
+				// If this component does not use state then show a direct delete button as we can not trash
+				JToolbarHelper::deleteList('', 'checks.delete', 'JTOOLBAR_DELETE');
+			}
+
+			if (isset($this->items[0]->state))
+			{
+				JToolbarHelper::divider();
+				JToolbarHelper::archiveList('checks.archive', 'JTOOLBAR_ARCHIVE');
+			}
+
+			if (isset($this->items[0]->checked_out))
+			{
+				JToolbarHelper::custom('checks.checkin', 'checkin.png', 'checkin_f2.png', 'JTOOLBAR_CHECKIN', true);
+			}
+		}
+
+		// Show trash and delete for components that uses the state field
+		if (isset($this->items[0]->state))
+		{
+			if ($state->get('filter.state') == -2 && $canDo->get('core.delete'))
+			{
+				JToolbarHelper::deleteList('', 'checks.delete', 'JTOOLBAR_EMPTY_TRASH');
+				JToolbarHelper::divider();
+			}
+			elseif ($canDo->get('core.edit.state'))
+			{
+				JToolbarHelper::trash('checks.trash', 'JTOOLBAR_TRASH');
+				JToolbarHelper::divider();
+			}
+		}
 
 		if ($canDo->get('core.admin'))
 		{
@@ -70,8 +133,25 @@ class Db8sitedevViewDashboard extends JViewLegacy
 		}
 
 		// Set sidebar action - New in 3.0
-		JHtmlSidebar::setAction('index.php?option=com_db8sitedev&view=dashboard');
+		JHtmlSidebar::setAction('index.php?option=com_db8sitedev&view=checks');
 
 		$this->extra_sidebar = '';
+	}
+
+	/**
+	 * Method to order fields 
+	 *
+	 * @return array
+	 */
+	protected function getSortFields()
+	{
+		return array(
+			'a.`title`' => JText::_('COM_DB8SITEDEV_CHECKS_TITLE'),
+			'a.`checked`' => JText::_('COM_DB8SITEDEV_CHECKS_CHECKED'),
+			'a.`catid`' => JText::_('COM_DB8SITEDEV_CHECKS_CATID'),
+			'a.`ordering`' => JText::_('JGRID_HEADING_ORDERING'),
+			'a.`state`' => JText::_('JSTATUS'),
+			'a.`id`' => JText::_('JGRID_HEADING_ID'),
+		);
 	}
 }
